fcdd4858f5f722941d4413ccd4831952d6cede61
Alexander Korolev <alexander.korolev.germany@gmail.com>
2021-04-15 23:03:48 +0200

    readability

--- tests/npm-build/build.rs
+++ tests/npm-build/build.rs
@@ -9,19 +9,16 @@ fn main() -> Result<()> {
     let out_dir = env::var("OUT_DIR").unwrap();
     let generated_file = Path::new(&out_dir).join("generated.in");
 
-    let version = if generated_file.exists() {
+    let version = 1 + if generated_file.exists() {
         fs::read_to_string(&generated_file)?
             .parse::<usize>()
             .unwrap()
     } else {
         0
-    } + 1;
+    };
 
     fs::write(generated_file, version.to_string())?;
-    fs::write(
-        "fixtures-04/package-lock.json",
-        r#"{"version":"0.1.0"}"#,
-    )?;
+    fs::write("fixtures-04/package-lock.json", r#"{"version":"0.1.0"}"#)?;
 
     Ok(())
 }
